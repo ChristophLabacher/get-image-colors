7052f3d2df21992a360063ff80785d515954143d
Christoph Labacher <christoph.labacher@gmail.com>
2016-06-01 13:06:59 +0200

    Add var for color count

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const getPixels = require('get-pixels')
 const getRgbaPalette = require('get-rgba-palette')
 const chroma = require('chroma-js')
 
-module.exports = function colorPalette (buffer, type, callback) {
+module.exports = function colorPalette (buffer, type, count, callback) {
   getPixels(buffer, type, function (err, pixels) {
     if (err) return callback(err)
-    const palette = getRgbaPalette(pixels.data, 5).map(function (rgba) {
+    const palette = getRgbaPalette(pixels.data, count).map(function (rgba) {
       return chroma(rgba)
     })
 
